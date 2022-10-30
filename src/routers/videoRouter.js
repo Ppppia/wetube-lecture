0999e54167e41ee7d0e2d4f7bea6b1bc2eb921b5
Pia <pia0413@gmail.com>
2022-10-31 00:07:22 +0900

    lecture #6 finished

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,19 @@
 import express from "express";
+import {
+  watch,
+  deleteVideo,
+  getUpload,
+  postUpload,
+  getEdit,
+  postEdit,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 
-videoRouter.get("/watch", handleWatchVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
