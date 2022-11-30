b3bfcc72a8bb8534dda91bd87e1f1bd28595fa96
Pia <pia0413@gmail.com>
2022-11-30 19:23:25 +0900

    hashtag

--- src/models/Video.js
+++ src/models/Video.js
@@ -15,9 +15,7 @@ const videoSchema = new mongoose.Schema({
 });
 
 videoSchema.static("formatHashtags", function (hashtags) {
-  return hashtags
-    .split(", ")
-    .map((word) => (word.startsWith("#") ? word.trim() : `#${word}`));
+  return hashtags.split(",").map((word) => word.trim());
 });
 
 const Video = mongoose.model("Video", videoSchema);
